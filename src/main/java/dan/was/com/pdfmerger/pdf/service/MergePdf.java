9ccfbb89c1ca0430402c9085a8daee36c36c0bb7
rancor <d.wasil@protonmail.com>
2021-04-10 12:25:34 +0200

    first junit test commit

--- src/main/java/dan/was/com/pdfmerger/pdf/service/MergePdf.java
+++ src/main/java/dan/was/com/pdfmerger/pdf/service/MergePdf.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -18,7 +19,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
+//@Component
+@Service
 public class MergePdf {
 
     @Autowired
---
+++ src/test/java/dan/was/com/pdfmerger/MvcLayerTest.java
@@ -0,0 +1,51 @@
+package dan.was.com.pdfmerger;
+
+import dan.was.com.pdfmerger.pdf.manipulation.DeleteFiles;
+import dan.was.com.pdfmerger.pdf.repository.JpaMergedPdfRepository;
+import dan.was.com.pdfmerger.pdf.repository.JpaUploadedPdfRepository;
+import dan.was.com.pdfmerger.pdf.service.MergePdf;
+import dan.was.com.pdfmerger.storage.service.MergedPdfDownloadService;
+import dan.was.com.pdfmerger.storage.service.PdfStorageService;
+import dan.was.com.pdfmerger.storage.service.UploadedPdfDownloadService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest
+public class MvcLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private MergePdf mergePdf;
+    @MockBean
+    private PdfStorageService pdfStorageService;
+    @MockBean
+    private MergedPdfDownloadService mergedPdfDownloadService;
+    @MockBean
+    private UploadedPdfDownloadService uploadedPdfDownloadService;
+    @MockBean
+    private JpaMergedPdfRepository jpaMergedPdfRepository;
+    @MockBean
+    private JpaUploadedPdfRepository jpaUploadedPdfRepository;
+    @MockBean
+    private DeleteFiles deleteFiles;
+
+
+    @Test
+    public void defaultResponse() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+    @Test
+    public void uploadResponse() throws Exception {
+        this.mockMvc.perform(post("/upload")).andDo(print()).andExpect(status().is3xxRedirection());
+    }
+}
